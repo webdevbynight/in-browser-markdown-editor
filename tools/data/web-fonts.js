cf3f591c5a0bc4ec436026b7da2bc6eaef19daed
webdevbynight <git@victor-brito.dev>
2024-10-20 03:39:37 +0200

    Added web fonts

---
+++ tools/data/web-fonts.js
@@ -0,0 +1,14 @@
+/**
+ *  The fonts and weights to install must be declared as follows:
+ * {
+ *      "Name_of_the_font": ["Bold", "Italic", "Regular"],
+ *      "Name_of_the_variable_font": ["Italic-VariableFont_wght", "VariableFont_wght"]
+ * }
+ */
+const webFontsToInstall = {
+    Roboto: ["Light", "Regular"],
+    Roboto_Mono: ["VariableFont_wght"],
+    Roboto_Slab: ["VariableFont_wght"]
+};
+
+export default webFontsToInstall;
