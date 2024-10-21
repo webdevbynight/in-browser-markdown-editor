d15c362c2dd61d45d32078cd764b709bbaa62aa5
webdevbynight <git@victor-brito.dev>
2024-10-21 12:32:30 +0200

    Implemented and styled modal dialog for deletion confirmation

--- ts/index.ts
+++ ts/index.ts
@@ -24,3 +24,46 @@ if (app && editor) {
         });
     }
 }
+
+// Show confirmation dialog before deleting
+
+// <!-- Begin modal dialog -->
+// <dialog class="confirmation" aria-live="assertive" open>
+//     <h2></h2>
+//     <p>Are you sure you want to delete the ‘welcome.md’ document and its contents? This action cannot be reversed.</p>
+//     <p>
+//         <button type="button" autofocus></button>
+//     </p>
+// </dialog>
+// <!-- End modal dialog -->
+const deleteButton = app?.querySelector<HTMLButtonElement>(`button[type="button"][title="Delete"]`);
+if (deleteButton) {
+    deleteButton.addEventListener("click", () => {
+        const documentNameInput = document.getElementById("document-name");
+        if (documentNameInput) {
+            const documentName = documentNameInput.dataset.documentName ?? "";
+            const dialog = document.createElement("dialog");
+            dialog.className = "confirmation";
+            dialog.setAttribute("aria-live", "assertive");
+            const h2 = document.createElement("h2");
+            h2.textContent = "Delete this document?";
+            const p = document.createElement("p");
+            p.innerText = `Are you sure you want to delete the ‘${documentName}’ document and its contents? This action cannot be reversed.`;
+            const buttonContainer = document.createElement("p");
+            const button = document.createElement("button");
+            button.type = "button";
+            button.autofocus = true;
+            button.textContent = "Confirm & Delete";
+            button.addEventListener("click", () => {
+                dialog.close();
+                dialog.remove();
+            });
+            buttonContainer.appendChild(button);
+            dialog.appendChild(h2);
+            dialog.appendChild(p);
+            dialog.appendChild(buttonContainer);
+            document.body.appendChild(dialog);
+            dialog.showModal();
+        }
+    });
+}
